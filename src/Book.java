41dce4cebe82e6db25519f935e4822ba2e3298a8
Fatemeh Rajabzade <ra.fatemeh851@gmail.com>
2025-03-25 05:33:20 -0700

    add Book class

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,31 @@
 public class Book {
+
+    private String title;
+    private int pageCount;
+
+    public Book(String title, int pageCount){
+        if (title == null){
+            throw new IllegalArgumentException("Title cannot be empty");
+        }
+        if (pageCount <= 0){
+            throw new IllegalArgumentException("Page count must be positive");
+        }
+        this.pageCount = pageCount;
+        this.title = title;
+    }
+    //getter های مناسب
+    public String getTitle(){
+        return title;
+    }
+
+    public int getPageCount(){
+        return pageCount;
+    }
+
+    //چاپ اسم کتاب و تعداد صفحات
+    @Override
+    public String toString(){
+        return title + "(" + pageCount + ")" ;
+    }
+
 }
