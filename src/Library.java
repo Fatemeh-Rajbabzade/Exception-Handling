2d2e66d1aa69fa7b5750b724c321ea49582a0ddd
Fatemeh Rajabzade <ra.fatemeh851@gmail.com>
2025-03-25 06:09:19 -0700

    Complete Library class

--- src/Library.java
+++ src/Library.java
@@ -1,2 +1,43 @@
+import java.util.ArrayList;
+
 public class Library {
+    private ArrayList<Book> books;
+
+    public Library(){
+        books = new ArrayList<>();
+    }
+
+    //add book
+    public void addBook(Book book) throws InvalidBookException{
+        if (book == null){
+            throw new InvalidBookException("Book should not be null.");
+        }
+        books.add(book);
+    }
+
+    //find book by title
+    private Book findBook(String title){
+        for (Book book : books){
+            if (book.getTitle().equals(title))
+                return book;
+            else
+                return null;
+        }
+    }
+
+    public void borrowBook(String title) throws BookNotFoundException, EmptyLibraryException{
+        if(books.size() == 0){
+            throw new EmptyLibraryException("Library is empty, no books to borrow");
+        }
+
+        Book book = findBook(title);
+        if (book == null){
+            throw new BookNotFoundException("Book with title" + title + "not found.");
+        }
+
+        System.out.println("Successfully borrowed the book : " + book.toString());
+
+    }
+
+
 }
