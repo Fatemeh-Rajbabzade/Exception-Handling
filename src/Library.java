d16adf52a36043fb5b58d12f2004bda7d6bdba35
Fatemeh Rajabzade <ra.fatemeh851@gmail.com>
2025-03-25 06:25:10 -0700

    Complete methods in Library class:

--- src/Library.java
+++ src/Library.java
@@ -36,8 +36,30 @@ public class Library {
         }
 
         System.out.println("Successfully borrowed the book : " + book.toString());
-
     }
 
+    //return book
+    public void returnBook(String title) throws BookNotFoundException{
+        Book book = findBook(title);
+        if (book == null){
+            throw new BookNotFoundException("Cannot return Book with title" + title + "not found.");
+        }
+        System.out.println("Successfullu returned : " +book.toString());
+    }
 
+    //Print info
+    public void listBook() throws EmptyLibraryException{
+        if (books.size() == 0)
+        {
+            throw new EmptyLibraryException("Library is empty.")
+        }
+        System.out.println("Book in the library:");
+        for (Book book : books){
+            System.out.println(book.toString());
+        }
+    }
+    
+    public ArrayList<Book> getBooks(){
+        return books;
+    }
 }
